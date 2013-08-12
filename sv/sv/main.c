39a25e84f62528df489fd9585f543a78804ca7e4
caothanhhai18 <caothanhhai18@gmail.com>
2013-08-13 03:35:02 +0700

    1208v2

--- sv/sv/main.c
+++ sv/sv/main.c
@@ -2,49 +2,88 @@
 #include <stdlib.h>
 #include <string.h>
 struct sv {
-    char tensv[5];
-    char mssv[5];
+    char tensv[10];
+    char mssv[10];
     float dtb;
     struct sv *link;
 };
 
 struct sv *head;
 
-void insert_end(){
+void insert_end(int i){
     struct sv *temp,*ptr;
     temp = (struct sv *)malloc(sizeof(struct sv));
     ptr=head;
+    while(1){
+        if(ptr->link!=NULL){
+            ptr=ptr->link;
+        }
+        else{
+            break;
+        }
+    }
+    printf("nhap tensv thu %d :\n",i);
+    scanf("%s",&(temp->tensv));
+    printf("nhap mssv thu %d :\n",i);
+    scanf("%s",&(temp->mssv));
+    printf("nhap dtb thu %d :\n",i);
+    scanf("%f",&(temp->dtb));
+    temp->link=NULL;
+    ptr->link=temp;
 
-        printf("nhap tensv thu 1 :\n");
-        scanf("%s",&(temp->tensv));
-        printf("nhap mssv thu %d :\n",1);
-        scanf("%s",&(temp->mssv));
-        printf("nhap dtb thu %d :\n",1);
-        scanf("%f",&(temp->dtb));
-        temp->link=NULL;
-        ptr->link=temp;
+}
+void insert_begin(){
+    struct sv *temp;
+    temp = (struct sv *)malloc(sizeof(struct sv));
+    printf("nhap tensv :\n");
+    scanf("%s",&(temp->tensv));
+    printf("nhap mssv :\n");
+    scanf("%s",&(temp->mssv));
+    printf("nhap dtb :\n");
+    scanf("%f",&(temp->dtb));
+
+    temp->link=head;
+    head=temp;
 
 }
 
-void create_lk(){
+void insert_pos(int pos){
+    struct sv *temp,*ptr;
+    int i;
+    temp = (struct sv *)malloc(sizeof(struct sv));
+    ptr=head;
+    for(i=0;i<pos;i++){
+        ptr=ptr->link;
+    }
+    printf("nhap tensv :\n");
+    scanf("%s",&(temp->tensv));
+    printf("nhap mssv :\n");
+    scanf("%s",&(temp->mssv));
+    printf("nhap dtb :\n");
+    scanf("%f",&(temp->dtb));
+    temp->link=ptr->link;
+    ptr->link=temp;
+}
+
+void create_lk(int no){
     struct sv *temp;
     int i;
-
     temp = (struct sv *)malloc(sizeof(struct sv));
+    for(i=0;i<no;i++){
         if(head==NULL){
-        printf("nhap tensv thu %d :\n",0);
+        printf("nhap tensv thu %d :\n",i);
         scanf("%s",&(temp->tensv));
-        printf("nhap mssv thu %d :\n",0);
+        printf("nhap mssv thu %d :\n",i);
         scanf("%s",&(temp->mssv));
-        printf("nhap dtb thu %d :\n",0);
+        printf("nhap dtb thu %d :\n",i);
         scanf("%f",&(temp->dtb));
         temp->link=NULL;
         head = temp;
-
         }
-
-
-
+        else{
+             insert_end(i);
+        }
+    }
 }
 
 print_lk(){
@@ -58,9 +97,9 @@ print_lk(){
 
 int main()
 {
-    create_lk();
-    insert_end();
-   // printf("%s",head->tensv);
-   print_lk();
+    create_lk(2);
+    //insert_begin();
+    insert_pos(1);
+    print_lk();
 
 }
